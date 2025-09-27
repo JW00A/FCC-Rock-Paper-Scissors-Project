19daebea7c90c36a8ec9c7c8178e3ed97b565164
JW00A <69700684+JW00A@users.noreply.github.com>
2025-09-27 18:20:54 -0400

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,4 +1,5 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+# This code was developed with help from Microsoft Copilot AI.
+
 import random
 import math
 
